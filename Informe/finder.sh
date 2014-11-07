2b6ae0ee23b7f8a774c71fcac808369b8e915466
Fernando Cladera <fcladera@fcladera.com>
2014-11-07 12:47:06 -0300

    Modificado diafragma -> servoactuador. Aclarado presion de aire sobre el servoactuador

--- Informe/finder.sh
+++ Informe/finder.sh
@@ -3,7 +3,7 @@
 echo "Script para buscar ocurrencias de palabras o expresiones"
 echo -e "Escribir en el array exprs las expresiones a buscar\n"
 
-expres=("ultimo" "Ultimo")
+expres=("aire" "Aire")
 
 files=(Abstract/abstract.tex Agradecimientos/Agradecimientos.tex Anexos/Anexo1-PuestaEnMarcha.tex 
 Anexos/Anexo2-Operacion.tex Bibliografia/bibliografia.bib Cap1-Introduccion/Cap1-Introduccion.tex 
@@ -55,4 +55,3 @@ do
 		grep -nr "$expre" $file
 	done
 done
-
