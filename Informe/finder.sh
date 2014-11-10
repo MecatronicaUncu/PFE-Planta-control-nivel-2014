4e82bd617a6831d64cae85af6f926fcdda9c10c5
Fernando Cladera <fcladera@fcladera.com>
2014-11-10 13:15:30 -0300

    Sustituciones valvula electroneumática -> de control y similares

--- Informe/finder.sh
+++ Informe/finder.sh
@@ -3,7 +3,7 @@
 echo "Script para buscar ocurrencias de palabras o expresiones"
 echo -e "Escribir en el array exprs las expresiones a buscar\n"
 
-expres=("[Mm]onitor")
+expres=("[gG]lobo")
 
 files=(Abstract/abstract.tex Agradecimientos/Agradecimientos.tex Anexos/Anexo1-PuestaEnMarcha.tex 
 Anexos/Anexo2-Operacion.tex Bibliografia/bibliografia.bib Cap1-Introduccion/Cap1-Introduccion.tex 
